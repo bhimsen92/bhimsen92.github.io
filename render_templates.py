b5018d9c24c6caf68194aaa15d1239ad16b7dc40
Bheemsen Kulkarni <bhimsen.pes@gmail.com>
2024-04-01 02:26:03 +0530

    fixed some issues

--- render_templates.py
+++ render_templates.py
@@ -11,25 +11,25 @@ POSTS_FOLDER = "posts"
 
 
 class CodeBlockExtension(Extension):
-    tags = {'code'}
-    
+    tags = {"code"}
+
     HTML_START = """
-    <!-- prettier-ignore-start -->
-    <div class="container mx-auto py-8">
-    <div class="bg-white rounded-lg shadow-md">
-        <pre class="p-2 rounded-lg overflow-x-auto">
-            <code class="block p-4 text-sm text-gray-800">
-    """
-    HTML_END = """
-        </code></pre></div></div>
-        <!-- prettier-ignore-end -->
-    """
-    
+    <div class="container mx-auto my-4">
+    <div class="bg-white rounded-lg shadow-md border border-gray-300">
+        <code class="block p-2 text-sm text-gray-900">
+        <pre class="p-2 rounded-lg overflow-x-auto">"""
+
+    HTML_END = """</pre></code></div></div>"""
+
     def parse(self, parser):
         lineno = next(parser.stream).lineno
-        body = parser.parse_statements(['name:endcode'], drop_needle=True)
-        return nodes.CallBlock(self.call_method('_render_block', []),
-                               [], [], body).set_lineno(lineno)
+        body = parser.parse_statements(["name:endcode"], drop_needle=True)
+
+        rendered_block = nodes.CallBlock(
+            self.call_method("_render_block", []), [], [], body
+        ).set_lineno(lineno)
+
+        return rendered_block
 
     def _render_block(self, caller):
         return self.HTML_START + caller() + self.HTML_END
@@ -37,11 +37,24 @@ class CodeBlockExtension(Extension):
 
 def to_blog_title(value):
     value_without_extension = os.path.splitext(value)[0]
-    return " ".join([w.capitalize() for w in re.split(r'[-_]', value_without_extension)])
+    return " ".join(
+        [w.capitalize() for w in re.split(r"[-_]", value_without_extension)]
+    )
 
 
 def to_html_extension(value):
     return value.replace(".jinja", ".html")
+
+
+def list_files_sorted_by_creation_time(directory):
+    return sorted(
+        [
+            file
+            for file in os.listdir(directory)
+            if file.endswith(".jinja") and file not in ["base.jinja", "layout.jinja"]
+        ],
+        key=lambda x: os.stat(os.path.join(directory, x)).st_ctime,
+    )
 
 
 @click.command()
@@ -49,26 +62,25 @@ def render_templates():
     os.makedirs(POSTS_FOLDER, exist_ok=True)
 
     jinja_env = Environment(loader=FileSystemLoader(TEMPLATE_FOLDER))
-    jinja_env.filters['to_blog_title'] = to_blog_title
-    jinja_env.filters['to_html_extension'] = to_html_extension
+    jinja_env.filters["to_blog_title"] = to_blog_title
+    jinja_env.filters["to_html_extension"] = to_html_extension
     jinja_env.add_extension(CodeBlockExtension)
 
-    template_files = [
-        f for f in os.listdir(TEMPLATE_FOLDER) if f.endswith(".jinja") and f not in ["base.jinja", "layout.jinja"]
-    ]
+    template_files = list_files_sorted_by_creation_time(TEMPLATE_FOLDER)
 
     for template_file in template_files:
         template = jinja_env.get_template(template_file)
-        rendered_template = template.render(template_files=template_files, skip_files=["about.jinja", "index.jinja"])
+        rendered_template = template.render(
+            template_files=template_files, skip_files=["about.jinja", "index.jinja"]
+        )
 
         output_file = os.path.join(POSTS_FOLDER, f"{template_file.split('.')[0]}.html")
 
         with open(output_file, "w") as f:
             f.write(rendered_template)
-        
+
         click.echo(f"Rendered {template_file} to {output_file}")
 
 
 if __name__ == "__main__":
     render_templates()
-
